a0dc2d9a2627e41655769c1d8a2a71973d97337b
Nguyễn Lê Hoàng Hưng <nguyenlehoanghung2006@gmail.com>
2026-09-17 19:57:19 +0700

    Change admin password from '9999' to '123'

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ function login(username, password) {
   if (!username || !password) {
     return false; // username hoặc password rỗng
   }
-     if (username === 'admin' && password === '9999') {
+     if (username === 'admin' && password === '123') {
     return true;
   }
   return false;
